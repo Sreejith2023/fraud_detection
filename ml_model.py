7c4fcffda90867f36918a6dda49eb976eb499984
Sreejith2023 <sreejith2023@gmail.com>
2021-12-21 23:29:45 +0530

    added 3 new ml models

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,11 @@
 
 print("adding all ml related code here")
+
+def model1():
+    print("adding code for ML model1")
+
+def model1():
+    print("adding code for ML model1")
+
+def model1():
+    print("adding code for ML model1")
